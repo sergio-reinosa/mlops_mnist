415864e9d6fcc18d9623a8f9b9ad81c4f8b9418b
helena <helenam.hauter@gmail.com>
2024-01-09 17:43:57 +0100

    fix train_cml file

--- mlops_mnist/train_cml.py
+++ mlops_mnist/train_cml.py
@@ -1,7 +1,13 @@
 # assume we have a trained model
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import torch
+from mlops_mnist.models.model import MyNeuralNetOne
+from mlops_mnist.train_model import get_train_dataset
+
+model = MyNeuralNetOne()
 preds, target = [], []
+train_dataloader = get_train_dataset(64)
 for batch in train_dataloader:
     x, y = batch
     probs = model(x)
@@ -12,8 +18,10 @@ target = torch.cat(target, dim=0)
 preds = torch.cat(preds, dim=0)
 
 report = classification_report(target, preds)
-with open("classification_report.txt", 'w') as outfile:
+with open("classification_report.txt", "w") as outfile:
     outfile.write(report)
 confmat = confusion_matrix(target, preds)
-disp = ConfusionMatrixDisplay(cm = confmat, )
-plt.savefig('confusion_matrix.png')
+disp = ConfusionMatrixDisplay(
+    cm=confmat,
+)
+plt.savefig("confusion_matrix.png")
