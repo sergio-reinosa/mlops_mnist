4acfca5422858f6ab9317ef994ba3794d749f359
helena <helenam.hauter@gmail.com>
2024-01-09 11:39:59 +0100

    debug path

--- tests/test_training.py
+++ tests/test_training.py
@@ -6,10 +6,12 @@ from mlops_mnist.train_model import train
 
 @pytest.mark.parametrize("lr,epochs,batch_size,model_name", [(0.001, 1, 64, "test.pt"), (0.01, 1, 32, "unicorn.pt"), (0.0001, 1, 128, "helloworld.pt")])
 def test_training(lr, epochs, batch_size, model_name):
+    print(os.getcwd())
     runner = CliRunner()
     runner.invoke(train, ["--lr", str(lr), "--epochs", str(epochs), "--batch_size", str(batch_size), "--model_name", model_name])
 
     path = os.path.join("models", model_name)
+    
     assert os.path.exists(path)
 
     # clean up 
