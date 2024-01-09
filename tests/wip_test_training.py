ab65b429b935e95752b5dfdb5b46dd4f4365f1c0
helena <helenam.hauter@gmail.com>
2024-01-09 12:17:25 +0100

    exclude training test

--- tests/wip_test_training.py
+++ tests/wip_test_training.py
@@ -5,7 +5,7 @@ from mlops_mnist.train_model import train
 
 
 @pytest.mark.parametrize("lr,epochs,batch_size,model_name", [(0.001, 1, 64, "test.pt"), (0.01, 1, 32, "unicorn.pt"), (0.0001, 1, 128, "helloworld.pt")])
-def test_training(lr, epochs, batch_size, model_name):
+def _test_training(lr, epochs, batch_size, model_name):
     print(os.getcwd())
     runner = CliRunner()
     runner.invoke(train, ["--lr", str(lr), "--epochs", str(epochs), "--batch_size", str(batch_size), "--model_name", model_name])
